ee1638e881d7287d18347842368d64f70d94a7ee
ybing <994978354@qq.com>
2016-11-08 13:07:52 +0800

    Initial Commit

---
+++ YBAFNetworking/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  YBAFNetworking
+//
+//  Created by 尚往文化 on 16/11/8.
+//  Copyright © 2016年 YBing. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ YBAFNetworking/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  YBAFNetworking
+//
+//  Created by 尚往文化 on 16/11/8.
+//  Copyright © 2016年 YBing. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
